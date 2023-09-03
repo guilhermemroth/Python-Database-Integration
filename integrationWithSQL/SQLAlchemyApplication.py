8c615447524756578934df3319caf226f4703312
Guilherme Roth <guilhermemroth@hotmail.com>
2023-09-02 21:34:40 -0300

    Update SQLAlchemyApplication.py
    creating a session to persist data in SQLite

--- integrationWithSQL/SQLAlchemyApplication.py
+++ integrationWithSQL/SQLAlchemyApplication.py
@@ -1,10 +1,15 @@
+#importando aa bibliotecas e funções
 import sqlalchemy
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
+
 
 Base = declarative_base()
 
@@ -34,10 +39,46 @@ class Address(Base):
     )
 
     def __repr__():
-        return f"Address(id={self.id}, email={self.email_address})"
+        return f"Address(id={self.id}, email_address={self.email_address})"
     
 
-print(User.__tablename__)
-print(Address.__table__)
+# print(User.__tablename__)
+# print(Address.__table__)
 
 
+# conexão com banco de dados
+
+engine = create_engine("sqlite://")
+
+# criando as classes como tabela no BD
+Base.metadata.create_all(engine)
+
+# investigando o esquema do BD
+insp = inspect(engine)
+print(insp.get_table_names())
+print(insp.default_schema_name)
+
+
+with Session(engine) as session:
+    guilherme = User(
+        name='guilherme',
+        full_name='Guilherme Roth',
+        address=[Address(email_address='guilhermerth@hotmail.com')]
+    )
+
+    fiodor = User(
+        name='fiódor',
+        full_name='Fiódor Dostoiévsky',
+        address=[Address(email_address='fiodor_dost@karamazov.com'),
+                 Address(email_address='fdostoievsky@raskólnikov.com')]
+    )
+
+    machado = User(
+        name='machado',
+        full_name='Joaquim Maria Machado de Assis'
+        )
+    
+    # enviando para o BD (persistência de dados)
+    session.add_all([guilherme, fiodor, machado])
+    
+    session.commit()
