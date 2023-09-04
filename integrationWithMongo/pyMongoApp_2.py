cbcb94f578613d71134ee3f17b5103342ea37f49
Guilherme Roth <guilhermemroth@hotmail.com>
2023-09-04 17:42:30 -0300

    Update pyMongoApp_2.py

--- integrationWithMongo/pyMongoApp_2.py
+++ integrationWithMongo/pyMongoApp_2.py
@@ -15,3 +15,23 @@ print(posts.count_documents({}))
 print(posts.count_documents({"author": "Mike"}))
 print(posts.count_documents({"tags": "insert"}))
 pprint.pprint(posts.find_one({"tags": "insert"}))
+
+print("\nRetrieving post collection info in an orderly manner")
+for post in posts.find({}).sort("date"):
+    pprint.pprint(post)
+
+result = db.profiles.create_index([('author', pym.ASCENDING)], unique=True)
+print(sorted(list(db.profiles.index_information())))
+
+user_profile_user = [
+    {"user_id": 211, "name": "Luke"},
+    {"user_id": 212, "name": "Padme"}
+]
+
+res = db.profile_user.insert_many(user_profile_user)
+
+collections = db.list_collection_names()
+
+print('\nCollections stored in mongodb')
+for collection in collections:
+    print(collection)
