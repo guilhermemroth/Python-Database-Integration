575e160cf126b6fe98a8f8d5aafce6acfa780bb7
Guilherme Roth <guilhermemroth@hotmail.com>
2023-09-06 18:19:29 -0300

    Update challenge_pt2.2.py

--- challenge/challenge_pt2.2.py
+++ challenge/challenge_pt2.2.py
@@ -5,4 +5,26 @@ import pprint
 bank = pym.MongoClient("mongodb+srv://guilhermemroth:BtnBHsHjrSOenfgi@cluster0.lhpu10f.mongodb.net/?retryWrites=true&w=majority")
 
 db = bank.accounts
-collection = db.test_collection
+clients = db.clients
+
+
+print("Returning all the clients\n")
+for post in clients.find():
+    pprint.pprint(post)
+    print("\n")
+
+print("Returning the count of items:\n")
+print(clients.count_documents({}))
+print("\n")
+print(clients.count_documents({"name": "fiodor"}))
+print("\n")
+print(clients.count_documents({"name": "dante"}))
+print("\n")
+
+print("Returning formated\n")
+pprint.pprint(clients.find_one({"address": "Rio de Janeiro"}))
+
+print("\nReturning sorted:\n")
+for post in clients.find({}).sort("balance"):
+    pprint.pprint(post)
+    print("\n")
